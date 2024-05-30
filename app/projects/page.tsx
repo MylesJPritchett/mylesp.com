fad1b2e8bdc6265977052f049b8314f7c9044513
Myles Pritchett <myles@unicard.com.au>
2024-05-30 16:59:16 +1000

    fix up some more things and removed the predone posts, added start of projects

---
+++ app/projects/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Projects',
+  description: 'Look at my Projects.',
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <BlogPosts />
+    </section>
+  )
+}
